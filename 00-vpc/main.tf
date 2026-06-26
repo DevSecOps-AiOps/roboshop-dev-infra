b3c48654ff2ef87f2cbfcc13a92b99de121e895e
Chinnu891 <chinnusali891@gmail.com>
2026-06-26 12:35:01 +0530

    .

---
+++ 00-vpc/main.tf
@@ -0,0 +1,7 @@
+module "vpc" {
+    # source = "../terraform-aws-vpc" 
+    source = "git::https://github.com/DevSecOps-AiOps/terraform-aws-vpc.git?ref=main"
+    project = var.project
+    environment = var.environment
+    is_peering_required = false
+}
